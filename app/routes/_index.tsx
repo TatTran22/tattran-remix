dc5f7e75e5e54bdc9cacc03d25f7961a248023d1
Tat Tran <tattran.dev@gmail.com>
2024-05-24 22:54:52 +0700

    chore: test D1 production connection

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,47 +1,24 @@
-import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare';
+import type { LoaderFunction } from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
-import { useEffect } from 'react';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'New Remix App' },
-    {
-      name: 'description',
-      content: 'Welcome to Remix! Using Vite and Cloudflare!',
-    },
-  ];
-};
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  return { message: 'Hello from loader data!' };
+export const loader: LoaderFunction = async ({ context, params }) => {
+  const { env, cf, ctx } = context.cloudflare;
+  const { results } = await env.DB.prepare('SELECT * FROM users')
+    // .bind(params.productId)
+    .all();
+  return json(results);
 };
 
 export default function Index() {
-  const { message } = useLoaderData<typeof loader>();
-
-  useEffect(() => {
-    console.log('Message from loader data:', message);
-  }, [message]);
-
+  const results = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/'
-            rel='noreferrer'
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Welcome to Remix</h1>
+      <div>
+        A value from D1:
+        <pre>{JSON.stringify(results)}</pre>
+      </div>
     </div>
   );
 }
